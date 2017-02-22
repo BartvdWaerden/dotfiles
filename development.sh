ba618f32e12907e55f3916e48cf6ddb62dc30e22
Bart van der Waerden <bart@rodesk.nl>
2017-02-22 16:49:24 +0100

    Add new global npm packeges.

--- development.sh
+++ development.sh
@@ -8,6 +8,10 @@ npm install --global gulp-cli
 echo "Installing Grunt"
 npm install -g grunt-cli
 
+# Install Webpack
+echo "Installing Webpack"
+npm install -g webpack
+
 # Install Bower
 echo "Installing Bower"
 npm install -g bower
@@ -15,6 +19,14 @@ npm install -g bower
 # Install Stylelint
 echo "Installing Stylelint CLI"
 npm install -g stylelint
+
+# Install Node Sass
+echo "Installing Node Sass"
+npm install -g node-sass
+
+# Install PostCSS
+echo "Installing PostCSS"
+npm install -g postcss
 
 # Install Sass
 echo "Installing Sass"
@@ -27,3 +39,4 @@ gem install jekyll
 # Install the Bundler gem
 echo "Installing Bundler"
 gem install bundler
+
